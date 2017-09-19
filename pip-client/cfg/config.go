1662982bda81a37828b58e91afe8405e59169478
james-xiang <jxiang@infoblox.com>
2017-09-19 11:35:02 -0700

    simple mcafee gRPC client/server

---
+++ pip-client/cfg/config.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2017 Infoblox Inc. All Rights Reserved.
+
+package cfg
+
+import (
+	"flag"
+
+	// "github.com/Infoblox-CTO/janus-common/log"
+	log "github.com/Sirupsen/logrus"
+)
+
+const (
+	defaultLogLevel      = "debug"
+	defaultTestData      = "test.data"
+	defaultTestResult    = "test.output"
+	defaultNumOfWorkers  = 16
+	defaultTestDuration  = 10
+	defaultServerAddress = "127.0.0.1:5368"
+)
+
+var (
+
+	// log and healthcheck cofig
+	logLevel     = flag.String("log-level", defaultLogLevel, "[debug | info | warning | error | fatal | panic] set log level")
+	testData     = flag.String("test-data", defaultTestData, "test data file contains list of domains")
+	testResult   = flag.String("test-result", defaultTestResult, "test result file contains test results")
+	numOfWokers  = flag.Int("num-of-workers", defaultNumOfWorkers, "Concurrent threads which are making queries")
+	testDuration = flag.Int("test-duration", defaultTestDuration, "seconds to run the test")
+
+	serverAddress = flag.String("server-addr", defaultServerAddress, "pip server address")
+)
+
+// AppConfig is a global config object used
+type AppConfig struct {
+	LogLevel      string
+	TestData      string
+	TestResult    string
+	NumOfWorkers  int
+	TestDuration  int
+	ServerAddress string
+}
+
+var globalConfig *AppConfig
+
+// Load the application configurworkDirations
+func Load() error {
+	flag.Parse()
+
+	globalConfig = &AppConfig{}
+	globalConfig.LogLevel = *logLevel
+	globalConfig.TestData = *testData
+	globalConfig.TestResult = *testResult
+	globalConfig.NumOfWorkers = *numOfWokers
+	globalConfig.TestDuration = *testDuration
+	globalConfig.ServerAddress = *serverAddress
+	return nil
+
+}
+
+// Config returns configuration info instance
+func Config() *AppConfig {
+	if globalConfig == nil {
+		log.Fatal("Configuration is not loaded")
+	}
+	return globalConfig
+}
---
+++ pip-server/main.go
@@ -0,0 +1,80 @@
+//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+package main
+
+// #cgo LDFLAGS: libts.a -lpthread -ldl -lrt -lssl -lcrypto
+// #include <stdlib.h>
+// #include "ts.h"
+// #include "test.h"
+import "C"
+
+import (
+	"net"
+	"unsafe"
+
+	log "github.com/Sirupsen/logrus"
+	pb "github.com/infobloxopen/themis/pipservice"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// func C.Init() int
+
+func init() {
+	_ = C.Init()
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Debug("enter SayHello()")
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func (s *server) GetCategories(ctx context.Context, in *pb.Request) (*pb.Response, error) {
+	log.Debug("enter GetCategories()")
+	url := in.GetQueryURL()
+
+	c_category := C.RateUrl(C.CString(url))
+
+	var category string
+	resp := pb.Response{}
+
+	if c_category != nil {
+		resp.Status = pb.Response_OK
+		category = C.GoString(c_category)
+		C.free(unsafe.Pointer(c_category))
+
+	} else {
+		resp.Status = pb.Response_NOTFOUND
+		category = "un-categorized"
+	}
+	resp.Categories = category
+
+	return &resp, nil
+}
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+
+	log.Infoln("Starting server on port: %s", port)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPIPServer(s, &server{})
+	//pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
